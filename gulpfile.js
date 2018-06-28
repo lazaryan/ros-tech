b79c77fc5a75d58bc1e58954bc76751083633752
lazaryan <lazaryan99@gmail.com>
2018-06-29 02:48:06 +0300

    Изменил структуру проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,22 @@
-"use strict";
-/*общие*/
-const gulp          = require('gulp');
-const browserSync   = require('browser-sync');
-const gulpif        = require('gulp-if');
-const plumber       = require('gulp-plumber');
-const rename        = require('gulp-rename');
-const combine       = require('stream-combiner2').obj;
+var gulp = require('gulp'),
+    //minifyCss = require('gulp-minify-css'),
+    less = require('gulp-less');
+    //concat = require('gulp-concat');
 
-/*styles*/
-const less          = require('gulp-less');
-const autoprefixer  = require('gulp-autoprefixer');
-const sourcemaps    = require('gulp-sourcemaps');
-const csso          = require('gulp-csso');
+var paths = {
+    less: ['./less/**/*.less']
+};
 
-/*js*/
-const uglify        = require('gulp-uglify-es').default;
-
-/*images*/
-const imgmin        = require('gulp-imagemin');
-
-/*html*/
-const htmlmin       = require('gulp-htmlmin');
-const fileinclude   = require('gulp-file-include')
-//const useref        = require('gulp-useref');
-
-// Автоперезагрузка при изменении файлов в папке `dist`:
-// Принцип: меняем файлы в `/src`, они обрабатываются и переносятся в `dist` и срабатывает автоперезагрузка.
-
-const IsDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV ==='development';
-
-// Это таск нужен только при локальной разработке.
-gulp.task('livereload', () => {
-    browserSync.create();
-
-    browserSync.init({
-        server: {
-            baseDir: 'dist'
-        },
-        files: [
-            'dist/**/*.*'
-        ]
-    });
-});
-
-gulp.task('styles', () => {
-    gulp.src('src/less/**/[^_]*.less')
-        .pipe(plumber())
-        .pipe(gulpif(IsDevelopment, sourcemaps.init()))
+gulp.task('less', function () {
+    return gulp.src('./less/**/*.less')
         .pipe(less())
-        .pipe(autoprefixer(['last 50 versions']))
-        .pipe(gulpif(IsDevelopment, sourcemaps.write()))
-        .pipe(gulpif(!IsDevelopment, csso({
-            restructure: true,
-            sourceMap: true,
-            debug: true
-        })))
-        .pipe(gulp.dest('./dist/styles'));
+        //.pipe(concat('style.css'))
+        //.pipe(minifyCss())
+        .pipe(gulp.dest('./styles/'));
 });
 
-gulp.task('img', () => {
-    gulp.src('src/images/**/*.*')
-        .pipe(gulpif(!IsDevelopment, imgmin({
-            interlaced: true
-        })))
-        .pipe(gulp.dest('./dist/images'));
+gulp.task('watcher',function(){
+    gulp.watch(paths.less, ['less']);
 });
 
-gulp.task('js', () => {
-    gulp.src('src/js/**/*.*')
-        .pipe(plumber())
-        .pipe(gulpif(!IsDevelopment, uglify()))
-        .pipe(gulp.dest('./dist/js'));
-});
-
-gulp.task('html', () => {
-    gulp.src('src/**/[^_]*.+(html|php)')
-        .pipe(fileinclude())
-        .pipe(gulpif(!IsDevelopment, htmlmin({
-            collapseWhitespace: true
-        })))
-        .pipe(gulp.dest('./dist'));
-});
-
-// Отслеживание изменений в файлах, нужно только при локальной разработке
-gulp.task('watch', () => {
-    gulp.watch('src/less/**/*.less', ['styles']);
-    gulp.watch('src/**/*.+(html|php)', ['html']);
-    gulp.watch('src/img/**/*.*', ['img']);
-    gulp.watch('src/js/**/*.*', ['js']);
-});
-
-gulp.task('default', ['styles', 'img','html', 'js', 'livereload', 'watch']);
-gulp.task('prod', ['styles', 'html', 'img', 'js']);
+gulp.task('default', ['less', 'watcher']);
