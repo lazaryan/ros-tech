b1b1eccb9a16171bf002146e7755418cc4e22569
lazaryan <lazaryan99@gmail.com>
2018-07-01 12:37:55 +0300

    Добавил поиск по сайту

--- js/header.js
+++ js/header.js
@@ -1,6 +1,18 @@
-let burger = document.querySelector('.js-header__logo_burger');
-let nav = document.querySelector('.js-header__nav');
+let burger 		= document.querySelector('.js-header__logo_burger');
+let nav 		= document.querySelector('.js-header__nav');
+
+let search 		= document.querySelector('.js-header__nav_search-img');
+let search_bar		= document.querySelector('.js-header__search-bar');
+let close_search_bar	= document.querySelector('.js-header__search-bar_close');
 
 burger.addEventListener('click', () => {
 	nav.classList.toggle('header__nav_active');
+});
+
+search .addEventListener('click', () => {
+	search_bar.classList.toggle('header__search-bar_active');
+});
+
+close_search_bar.addEventListener('click', () => {
+	search_bar.classList.remove('header__search-bar_active');
 });
